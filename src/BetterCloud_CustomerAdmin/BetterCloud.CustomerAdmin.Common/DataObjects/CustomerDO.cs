07b9bb5b9d75b1c33ce2f9cd0c60592ee1937e15
andythecoderman <andy.batchelor@tritondigital.com>
2015-03-19 19:48:49 -0400

    added common project

---
+++ src/BetterCloud_CustomerAdmin/BetterCloud.CustomerAdmin.Common/DataObjects/CustomerDO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BetterCloud.CustomerAdmin.Common.DataObjects
+{
+    /// <summary>
+    /// POCO for containing customer information
+    /// </summary>
+    public class CustomerDO : DataObject
+    {
+    }
+}
---
+++ src/BetterCloud_CustomerAdmin/BetterCloud.CustomerAdmin.Common/DataObjects/DataObject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BetterCloud.CustomerAdmin.Common.DataObjects
+{
+    /// <summary>
+    /// Base class for all POCOs
+    /// </summary>
+    public abstract class DataObject
+    {
+    }
+}

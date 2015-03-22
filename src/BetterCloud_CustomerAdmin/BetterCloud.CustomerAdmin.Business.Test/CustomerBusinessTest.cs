bb0f103edf91a90311231ffc578155465266330b
andythecoderman <andy.batchelor@tritondigital.com>
2015-03-22 18:51:42 -0400

    data manipulation and buisiness methods outlined

--- src/BetterCloud_CustomerAdmin/BetterCloud.CustomerAdmin.Business.Test/CustomerBusinessTest.cs
+++ src/BetterCloud_CustomerAdmin/BetterCloud.CustomerAdmin.Business.Test/CustomerBusinessTest.cs
@@ -1,11 +1,40 @@
 ﻿using System;
+using BetterCloud.CustomerAdmin.Common.Interfaces.Business;
+using BetterCloud.CustomerAdmin.Common.Interfaces.DataAccess;
+using BetterCloud.CustomerAdmin.DataAccess.MSSQL;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ninject;
 
 namespace BetterCloud.CustomerAdmin.Business.Test
 {
     [TestClass]
     public class CustomerBusinessTest
     {
-        
+        private readonly IKernel _diKernel = new StandardKernel();
+
+        [TestInitialize]
+        public void Init()
+        {
+            _diKernel.Bind<ICustomerBusiness>().To<CustomerBusiness>();
+            _diKernel.Bind<ICustomerData>().To<CustomerDAO>();
+        }
+
+        public void GetCustomer_Existing()
+        {
+            try
+            {
+                var cuBiz = _diKernel.Get<ICustomerBusiness>();
+                var custDO = cuBiz.GetCustomer(Guid.Parse("844CB884-43C8-4B59-90CA-F07A3310095E")); //TODO: Mock Data
+                Assert.IsNotNull(custDO);
+            }
+            catch (AssertFailedException)
+            {
+                throw;
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.Message);
+            }    
+        }
     }
 }
--- src/BetterCloud_CustomerAdmin/BetterCloud.CustomerAdmin.Common/Interfaces/Business/ICustomerBusiness.cs
+++ src/BetterCloud_CustomerAdmin/BetterCloud.CustomerAdmin.Common/Interfaces/Business/ICustomerBusiness.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using BetterCloud.CustomerAdmin.Common.DataObjects;
 
 namespace BetterCloud.CustomerAdmin.Common.Interfaces.Business
 {
@@ -11,6 +9,37 @@ namespace BetterCloud.CustomerAdmin.Common.Interfaces.Business
     /// </summary>
     public interface ICustomerBusiness : IDisposable
     {
+        /// <summary>
+        /// Returns the full list of all Customers in the system
+        /// </summary>
+        /// <returns></returns>
+        List<CustomerDO> GetAllCustomers();
 
+        /// <summary>
+        /// Returns a single Customer based on the CustomerId
+        /// </summary>
+        /// <param name="customerId"></param>
+        /// <returns></returns>
+        CustomerDO GetCustomer(Guid customerId);
+
+        /// <summary>
+        /// Creates a new Customer record and returns the new CustomerId
+        /// </summary>
+        /// <param name="customerDO"></param>
+        /// <returns></returns>
+        Guid CreateCustomer(CustomerDO customerDO);
+
+        /// <summary>
+        /// Replaces the existing Customer record with the <param name="customerDO"></param> with the matching CustomerId property
+        /// </summary>
+        /// <param name="customerDO"></param>
+        /// <returns></returns>
+        void UpdateCustomer(CustomerDO customerDO);
+
+        /// <summary>
+        /// Purges the customer record with CustomerId matching the <param name="customerId"></param> argument
+        /// </summary>
+        /// <param name="customerId"></param>
+        void DeleteCustomer(Guid customerId);
     }
 }
